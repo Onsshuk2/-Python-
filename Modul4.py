1d345d23344710892830eb69b4477fa5113ea6ce
Onsshuk2 <onsshuk086@gmail.com>
2023-12-14 12:59:02 +0200

    first commit

---
+++ Modul4.py
@@ -0,0 +1,41 @@
+#EX1
+# num1=int(input("Enter number 1:"))
+# num2=int(input("Enter number 2:"))
+# for i in range(num1,num2+1):
+#     if i%7==0:
+#         print(i)
+#EX2
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+count=0
+for i in range(num1,num2+1):
+   print(i)
+for i in range(num2+1,num1,-1):
+    print(i)
+for i in range(num1,num2+1):
+    if i%7==0:
+        print(i)
+for i in range(num1,num2+1):
+    if i%5==0:
+        count+=1
+print(count)
+EX3
+# num1=int(input("Enter number 1:"))
+# num2=int(input("Enter number 2:"))
+# for i in range(num1,num2+1):
+#     if i%3==0:
+#         print("Fizz",i)
+# for i in range(num1,num2+1):
+#     if i%5==0:
+#         print("Buzz",i)
+# for i in range(num1,num2+1):
+#     if i%5==0 and i%3==0:
+#         print("Fizz Buzz",i) 
+# for i in range(num1,num2+1):
+#     if i%5!=0 and i%3!=0:
+#         print(i) 
+
+
+
+
+
